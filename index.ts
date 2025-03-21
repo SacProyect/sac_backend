2c223897e1755b9f972392b5ce34c6a4ec59fd6a
VictorDevCode21 <victorenrique2002@gmail.com>
2025-03-20 22:48:47 -0400

    solving cors error

--- index.ts
+++ index.ts
@@ -15,7 +15,14 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+// CORS configuration allowing only the frontend (localhost:5173)
+app.use(
+    cors({
+        origin: 'http://localhost:5173', // Replace with your frontend URL
+        methods: 'GET, POST, PUT, DELETE', // Add the HTTP methods you need
+        allowedHeaders: 'Content-Type, Authorization', // Allow headers you need
+    })
+);
 app.use("/user", userRouter)
 app.use("/taxpayer", taxpayerRouter)
 app.use("/reports", reportRouter)
