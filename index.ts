5fc92f0caa12d7027c8e75ddb1f1f7780048f755
Victor <victorenrique2002@gmail.com>
2025-04-29 23:25:44 -0400

    changes to listen to any ip

--- index.ts
+++ index.ts
@@ -59,7 +59,7 @@ app.use('/uploads', express.static(path.join(__dirname, './uploads')));
 
 
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is listening on port: ${PORT}`)
 })
 
